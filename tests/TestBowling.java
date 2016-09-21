2cb138e879a541108458d810e283ff889c7092dd
somename <someemail>
2016-09-21 15:35:30 +0300

    besouro automatic message

--- tests/TestBowling.java
+++ tests/TestBowling.java
@@ -41,7 +41,7 @@ public class TestBowling {
 		
 		assertEquals(game.score(), 14);
 	}
-	
+
 	@Test
 	public void verifyBowlingGame_Frames_3_Spare3_Score_29() throws BowlingException {
 		BowlingGame game = new BowlingGame();
@@ -49,9 +49,24 @@ public class TestBowling {
 		game.addFrame(new Frame(1, 4));
 		game.addFrame(new Frame(4, 5));
 		game.addFrame(new Frame(6, 4));
+		
 		game.addFrame(new Frame(5, 5));
 		
 		assertEquals(game.score(), 29);
+	}
+	
+	@Test
+	public void verifyBowlingGame_Frames_3_Spare34_Score_49() throws BowlingException {
+		BowlingGame game = new BowlingGame();
+
+		game.addFrame(new Frame(1, 4));
+		game.addFrame(new Frame(4, 5));
+		game.addFrame(new Frame(6, 4));
+		game.addFrame(new Frame(5, 5));
+		
+		game.addFrame(new Frame(10, 0));
+		
+		assertEquals(game.score(), 49);
 	}
 	
 	@Test
